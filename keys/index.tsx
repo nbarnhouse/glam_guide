95432a5d9d02cffcafbd5f271199989a40ed8271
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-02-18 22:49:57 -0800

    added iosUrlScheme in app.json

--- keys/index.tsx
+++ keys/index.tsx
@@ -1,3 +1,5 @@
 // Client ID - Web App: 1040286814755-jti1t0uis82ctifh5b7m9348q6704pba.apps.googleusercontent.com 
 
-// Client ID - Android: 
+// Client ID - iOS: 1040286814755-ge9q4rufrd144br0vfih4jlql44tmbjn.apps.googleusercontent.com 
+
+// Client ID - Android:
