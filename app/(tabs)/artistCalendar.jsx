23bec99f99aa03728d263845e211707785e0fad6
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-03-01 21:39:25 -0800

    created containers for calendar

--- app/(tabs)/artistCalendar.jsx
+++ app/(tabs)/artistCalendar.jsx
@@ -1,21 +1,49 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const ArtistCalendar = () => {
   return (
     <SafeAreaView
       style={{
-        flex: 1,
-        justifyContent: "center",
+        flex: 1, 
         alignItems: "center",
       }}
     >
-      <View>
-
+      <View style={styles.containerTop}>
+        <Text>
+          dfsddfghfdgjhdfgjfdj
+        </Text>
       </View>
+
+      <View style={styles.containerBottom}>
+        <Text>
+          dfsd
+        </Text>
+      </View>
+
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  containerTop: {
+    flex: 1,
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  containerBottom: {
+    flex: 3,
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+});
+
 
 export default ArtistCalendar;
