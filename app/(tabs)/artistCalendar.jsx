eb3e5ccca8eb2dea7d3dd06a4262d98b2870e4c9
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-03-01 22:27:03 -0800

    added sticky header and scrolling bottom container

--- app/(tabs)/artistCalendar.jsx
+++ app/(tabs)/artistCalendar.jsx
@@ -5,6 +5,8 @@ import {
   StyleSheet,
   TouchableOpacity,
   Alert,
+  ScrollView,
+  StatusBar,
 } from "react-native";
 import React from "react";
 import { getRelativeCoords } from "react-native-reanimated";
@@ -19,26 +21,32 @@ const ArtistCalendar = () => {
       }}
     >
       {/* Top Container */}
-      <View
-        style={styles.containerTop}>
-        <Text
-          style={{
-            fontWeight: '700',
-            fontSize: 18,
-          }}
+      <View style={styles.containerTop}>
+        <ScrollView
+          stickyHeaderIndices={[1]}
+          showsVerticalScrollIndicator={true}
         >
-          Calendar
-        </Text>
-        <Text>
-          Welcome back, Suah!
-        </Text>
+          <Text
+            style={{
+              fontWeight: '700',
+              fontSize: 18,
+            }}
+          >
+            Calendar
+          </Text>
+          <Text>
+            Welcome back, Suah!
+          </Text>
+        </ScrollView>  
       </View>
 
       {/* Bottom Container */}
       <View style={styles.containerBottom}>
-        <Text>
-          dfsd
-        </Text>
+        <ScrollView>
+          <Text style={{fontSize: 35}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </ScrollView>
       </View>
 
     </SafeAreaView>
